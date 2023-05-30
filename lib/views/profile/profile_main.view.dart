07d09ca9101be05adfa499461ff1d1f8b420ab95
Johannes Wirbser <jo391wir@htwg-konstanz.de>
2023-05-30 11:09:24 +0200

    added design from item_list_view to profile setings

--- lib/views/profile/profile_main.view.dart
+++ lib/views/profile/profile_main.view.dart
@@ -35,7 +35,48 @@ class ProfileMain extends StatelessWidget {
                   ),
                   margin: const EdgeInsets.symmetric(vertical: 13, horizontal: 20),
                   child: Card(
-                    child: Text(items[index]),
+                    margin: EdgeInsets.zero,
+                    elevation: 0,
+                    clipBehavior: Clip.hardEdge,
+                    child: InkWell(
+                      onTap: null,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 3, vertical: 3),
+                        child: SizedBox(
+                          height: 90,
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(7),
+                                  bottomLeft: Radius.circular(7),
+                                ),
+                                child: Image(
+                                  image: AssetImage("assets/images/default.jpg"),
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                              const SizedBox(width: 20),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      items[index],
+                                      style: const TextStyle(fontWeight: FontWeight.w700),
+                                      maxLines: 1,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 );
               },
