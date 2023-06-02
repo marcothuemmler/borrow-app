914bb65c926fbe94da4e1468184228cab8df1493
marcothuemmler <marco.thuemmler@me.com>
2023-06-02 12:36:46 +0200

    Fix text overflow

--- lib/views/chat/chat.view.dart
+++ lib/views/chat/chat.view.dart
@@ -30,6 +30,10 @@ class ChatView extends ConsumerWidget {
                       ChatBubble(isOwnMessage: true, message: "hallo"),
                       ChatBubble(isOwnMessage: false, message: "hallo"),
                       ChatBubble(
+                          isOwnMessage: false,
+                          message:
+                              "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."),
+                      ChatBubble(
                         isOwnMessage: true,
                         message: "Was letzte Preis?",
                       ),
--- lib/widgets/various_components/chat_bubble.widget.dart
+++ lib/widgets/various_components/chat_bubble.widget.dart
@@ -16,31 +16,40 @@ class ChatBubble extends StatelessWidget {
       mainAxisAlignment:
           isOwnMessage ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 4),
-          padding: const EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 20,
-          ),
-          decoration: BoxDecoration(
-            color: isOwnMessage
-                ? const Color.fromRGBO(60, 120, 240, 1)
-                : const Color.fromRGBO(240, 240, 240, 1),
-            borderRadius: const BorderRadius.all(Radius.circular(13)),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.black38,
-                blurRadius: 2,
-                offset: Offset(0, 1),
-              )
-            ],
-          ),
-          child: Text(
-            message,
-            style: TextStyle(
-              color: isOwnMessage ? Colors.white : Colors.black,
-              fontSize: 16,
-              fontWeight: FontWeight.w300,
+        Flexible(
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 4),
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 20,
+            ),
+            decoration: BoxDecoration(
+              color: isOwnMessage
+                  ? const Color.fromRGBO(60, 120, 240, 1)
+                  : const Color.fromRGBO(240, 240, 240, 1),
+              borderRadius: const BorderRadius.all(Radius.circular(13)),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black38,
+                  blurRadius: 2,
+                  offset: Offset(0, 1),
+                )
+              ],
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Flexible(
+                  child: Text(
+                    message,
+                    style: TextStyle(
+                      color: isOwnMessage ? Colors.white : Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
