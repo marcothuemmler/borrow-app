027999ddb080e75d8c61ff58bf3fa96fb0923423
Johannes Wirbser <jo391wir@htwg-konstanz.de>
2023-06-22 16:23:41 +0200

    added floatingActionButton to profileItemListView

--- lib/views/dashboard/profile/profile_item_list/profile_item_list.view.dart
+++ lib/views/dashboard/profile/profile_item_list/profile_item_list.view.dart
@@ -4,7 +4,6 @@ import "package:borrow_app/services/routing/routes.dart";
 import "package:borrow_app/views/dashboard/item_list/item_list.model.dart";
 import "package:borrow_app/views/dashboard/profile/categories_settings/category_settings.model.dart";
 import "package:borrow_app/views/dashboard/profile/profile_item_list/profile_item_list.model.dart";
-import "package:borrow_app/widgets/buttons/dotted_border_button.widget.dart";
 import "package:borrow_app/widgets/cards/item_card.widget.dart";
 import "package:borrow_app/widgets/dropdowns/dropdown.widget.dart";
 import "package:flutter/material.dart";
@@ -33,74 +32,69 @@ class ProfileItemListView extends ConsumerWidget with CategoryDialogMixin {
 
     final List<ItemListItemModel> filteredItems = model.filteredItems;
 
-    return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          if (filteredItems.isNotEmpty)
-            Expanded(
-              child: Column(
-                children: <Widget>[
-                  getDropDownMenu(controller, model, context),
-                  Expanded(
-                    child: ListView.builder(
-                      padding: const EdgeInsets.only(top: 20),
-                      itemCount: model.filteredItems.length,
-                      shrinkWrap: true,
-                      itemBuilder: (BuildContext context, int index) {
-                        final ItemListItemModel item =
-                            model.filteredItems.elementAt(index);
-                        return ItemCard(
-                          item: item,
-                          onTap: () => controller.navigateToItem(
-                            itemId: item.id,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () => context.pushNamed(
-                        newItemRoute.name,
-                        pathParameters: <String, String>{"groupId": groupId},
-                      ),
-                      child: Text(AppLocalizations.of(context).addNewItem),
-                    ),
-                  )
-                ],
-              ),
-            )
-          else
-            Expanded(
-              child: Column(
-                children: <Widget>[
-                  getDropDownMenu(controller, model, context),
-                  Center(
-                    child: Text(
-                      AppLocalizations.of(context).emptyGroupMessage,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 24,
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            if (filteredItems.isNotEmpty)
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    getDropDownMenu(controller, model, context),
+                    Expanded(
+                      child: ListView.builder(
+                        padding: const EdgeInsets.only(top: 20),
+                        itemCount: model.filteredItems.length,
+                        shrinkWrap: true,
+                        itemBuilder: (BuildContext context, int index) {
+                          final ItemListItemModel item =
+                              model.filteredItems.elementAt(index);
+                          return ItemCard(
+                            item: item,
+                            onTap: () => controller.navigateToItem(
+                              itemId: item.id,
+                            ),
+                          );
+                        },
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 25),
-                  DottedBorderButton(
-                    title: AppLocalizations.of(context).addNewItem,
-                    icon: const Icon(Icons.add),
-                    onTap: () {
-                      context.pushNamed(
-                        newItemRoute.name,
-                        pathParameters: <String, String>{"groupId": groupId},
-                      );
-                    },
-                    width: 200,
-                  ),
-                ],
+                  ],
+                ),
+              )
+            else
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    getDropDownMenu(controller, model, context),
+                    Center(
+                      child: Text(
+                        AppLocalizations.of(context).emptyGroupMessage,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                  ],
+                ),
               ),
-            ),
-        ],
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.add, size: 26),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        onPressed: () => {
+          context.pushNamed(
+            newItemRoute.name,
+            pathParameters: <String, String>{"groupId": groupId},
+          )
+        },
+        label: Text(AppLocalizations.of(context).addNewItem),
       ),
     );
   }
