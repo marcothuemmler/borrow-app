72ca57dac4380bd78acc15791f508d51db14355b
marcothuemmler <marco.thuemmler@me.com>
2023-06-28 11:31:42 +0200

    Remove group from group_selection.view after leaving it

--- lib/widgets/dialogs/leave_group_bottom_sheet.dialog.dart
+++ lib/widgets/dialogs/leave_group_bottom_sheet.dialog.dart
@@ -1,5 +1,6 @@
 import "package:borrow_app/common/providers.dart";
 import "package:borrow_app/services/routing/routes.dart";
+import "package:borrow_app/views/dashboard/profile/group_settings/group_settings.model.dart";
 import "package:borrow_app/views/dashboard/profile/group_settings/group_settings.view.dart";
 import "package:flutter/material.dart";
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
@@ -17,6 +18,12 @@ class LeaveGroupBottomSheet extends ConsumerWidget {
     final GroupSettingsController controller = ref.read(
       providers.groupSettingsControllerProvider(_groupId).notifier,
     );
+    final GroupSettingsModel model = ref.watch(
+      providers.groupSettingsControllerProvider(_groupId),
+    );
+    if (model.isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
     return SafeArea(
       child: ConstrainedBox(
         constraints: const BoxConstraints(maxWidth: 500),
@@ -45,6 +52,12 @@ class LeaveGroupBottomSheet extends ConsumerWidget {
                 onPressed: () async {
                   final bool success = await controller.leaveGroup();
                   if (success && controller.mounted) {
+                    if (ref.exists(
+                      providers.groupSelectionControllerProvider,
+                    )) {
+                      // ignore: unused_result
+                      ref.refresh(providers.groupSelectionControllerProvider);
+                    }
                     context.goNamed(groupSelectionRoute.name);
                   }
                 },
