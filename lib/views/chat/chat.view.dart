1508e3b866cec6ebab9f10cd73b0b0c9bb0011d2
marcothuemmler <marco.thuemmler@me.com>
2023-06-30 19:27:23 +0200

    Small layout changes. Refresh chat list if mounted when disposing chat

--- lib/views/chat/chat.view.dart
+++ lib/views/chat/chat.view.dart
@@ -1,6 +1,7 @@
 import "package:borrow_app/common/providers.dart";
 import "package:borrow_app/views/chat/chat.model.dart";
 import "package:borrow_app/widgets/various_components/chat_bubble.widget.dart";
+import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
@@ -30,73 +31,72 @@ class ChatView extends ConsumerWidget {
     WidgetsBinding.instance.addPostFrameCallback(_scrollDown);
     return Scaffold(
       appBar: AppBar(title: const Text("Title")),
-      body: ColoredBox(
-        color: const Color.fromRGBO(240, 240, 240, 1),
-        child: SafeArea(
-          child: Stack(
-            fit: StackFit.expand,
-            alignment: Alignment.bottomCenter,
-            children: <Widget>[
-              ColoredBox(
-                color: Theme.of(context).scaffoldBackgroundColor,
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      child: ListView.builder(
-                        physics: const BouncingScrollPhysics(),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 15,
-                        ),
-                        controller: _scrollController,
-                        itemCount: model.messages.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return ChatBubble(
-                            message: model.messages.elementAt(index),
-                          );
-                        },
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 8,
-                        horizontal: 16,
-                      ),
-                      color: const Color.fromRGBO(240, 240, 240, 1),
-                      child: Row(
-                        children: <Widget>[
-                          Flexible(
-                            child: TextField(
-                              autofocus: true,
-                              keyboardType: TextInputType.multiline,
-                              controller: _textController,
-                              maxLines: null,
-                              decoration: const InputDecoration(
-                                filled: true,
-                                border: OutlineInputBorder(),
-                                fillColor: Colors.white,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          IconButton(
-                            onPressed: () {
-                              controller.sendMessage(
-                                message: _textController.text.trim(),
-                              );
-                              _textController.clear();
-                            },
-                            icon: const Icon(Icons.send),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: SafeArea(
+              bottom: false,
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 15,
                 ),
+                controller: _scrollController,
+                itemCount: model.messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ChatBubble(
+                    message: model.messages.elementAt(index),
+                  );
+                },
               ),
-            ],
+            ),
           ),
-        ),
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 16,
+            ),
+            decoration: BoxDecoration(
+              color: CupertinoColors.systemGrey6,
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  blurRadius: 2,
+                  color: Colors.black.withOpacity(0.16),
+                )
+              ],
+            ),
+            child: SafeArea(
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    child: TextField(
+                      autofocus: true,
+                      keyboardType: TextInputType.multiline,
+                      controller: _textController,
+                      maxLines: null,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        border: OutlineInputBorder(),
+                        fillColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  IconButton(
+                    onPressed: () {
+                      controller.sendMessage(
+                        message: _textController.text.trim(),
+                      );
+                      _textController.clear();
+                    },
+                    icon: const Icon(Icons.send),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
