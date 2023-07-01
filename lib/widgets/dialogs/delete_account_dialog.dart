e8526cd4e17cefe3ee13ad95f64e802ba6f975f9
marcothuemmler <marco.thuemmler@me.com>
2023-07-01 16:22:21 +0200

    Layout fixes and QOL changes

--- lib/widgets/dialogs/delete_account_dialog.dart
+++ lib/widgets/dialogs/delete_account_dialog.dart
@@ -6,12 +6,13 @@ import "package:flutter_gen/gen_l10n/app_localizations.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
 class DeleteAccountDialog extends ConsumerWidget {
-  const DeleteAccountDialog({super.key});
+  DeleteAccountDialog({super.key});
+
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final TextEditingController _textController = TextEditingController();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final GlobalKey<FormState> formKey = GlobalKey();
-    final TextEditingController textEditingController = TextEditingController();
     final ProfileSettingsController controller =
         ref.read(providers.profileSettingsControllerProvider.notifier);
     return SafeArea(
@@ -19,53 +20,55 @@ class DeleteAccountDialog extends ConsumerWidget {
         constraints: const BoxConstraints(maxWidth: 500),
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                "${AppLocalizations.of(context).deleteAccount}?",
-                style: const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w600,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  "${AppLocalizations.of(context).deleteAccount}?",
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 40),
-              Text(
-                AppLocalizations.of(context).accountDeletionMessage,
-                style: const TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 30),
-              ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth: 350),
-                child: Form(
-                  key: formKey,
-                  child: TextFormField(
-                    validator: (String? value) => controller.validateFormField(
-                      fieldType: FormValidationType.password,
-                      context: context,
-                      value: value,
-                    ),
-                    controller: textEditingController,
-                    decoration: InputDecoration(
-                      label: Text(AppLocalizations.of(context).insertPassword),
-                      border: const OutlineInputBorder(),
+                const SizedBox(height: 40),
+                Text(
+                  AppLocalizations.of(context).accountDeletionMessage,
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 30),
+                ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 350),
+                  child: Form(
+                    key: _formKey,
+                    child: TextFormField(
+                      validator: (String? value) =>
+                          controller.validateFormField(
+                        fieldType: FormValidationType.password,
+                        context: context,
+                        value: value,
+                      ),
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        label:
+                            Text(AppLocalizations.of(context).insertPassword),
+                        border: const OutlineInputBorder(),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              TextButton(
-                onPressed: () {
-                  if (formKey.currentState!.validate()) {
-                    controller.deleteAccount(
-                      password: textEditingController.text,
-                    );
-                  }
-                },
-                style: TextButton.styleFrom(foregroundColor: Colors.red),
-                child: Text(AppLocalizations.of(context).deleteAccount),
-              ),
-            ],
+                const SizedBox(height: 30),
+                TextButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      controller.deleteAccount(password: _textController.text);
+                    }
+                  },
+                  style: TextButton.styleFrom(foregroundColor: Colors.red),
+                  child: Text(AppLocalizations.of(context).deleteAccount),
+                ),
+              ],
+            ),
           ),
         ),
       ),
